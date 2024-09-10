16e6051027751820fe34c4464e5ef5be12050c8a
kylieb233 <145594381+kylieb233@users.noreply.github.com>
2024-09-10 09:37:39 -0500

    oop

--- ProductApp/ProductApp/ViewController.swift
+++ ProductApp/ProductApp/ViewController.swift
@@ -16,14 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputIO2: UITextField!
     
     
+  
     @IBOutlet weak var outputIO: UILabel!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func submitBtn(_ sender: Any) {
+        var num1 = Int (inputIO1.text!)
+        var num2 = Int (inputIO2.text!)
+  
+        
+        var product = num1! * num2!
+        outputIO.text = "\(product)"
     }
     
 }
