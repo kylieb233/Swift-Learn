24b0902811851f0e3db2439f772381529d734bfc
kylieb233 <145594381+kylieb233@users.noreply.github.com>
2024-09-10 10:48:30 -0500

    new app

---
+++ DiscountApp/DiscountApp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  DiscountApp
+//
+//  Created by Beck,Kylie J on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputOL: UITextField!
+    
+    @IBOutlet weak var outputOL: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func calculateBtn(_ sender: Any) {
+        
+        var amount =  Double (inputOL.text!)
+        if(amount! > 0) &&
+            (amount! < 50){
+            var newAmount = (amount! * 10/100)
+            outputOL.text = ("You recieved 10% discount on the amount \(amount!) .The discount value is \(newAmount)")
+        }else if (amount! > 51) &&
+                (amount! < 100){
+            var newAmount = (amount! * 20/100)
+            outputOL.text = ("You recieved 20% discount on the amount \(amount!) .The discount value is \(newAmount)")
+        }
+        else {
+            var newAmount = (amount! * 30/100)
+            outputOL.text = (("You recieved 30% discount on the amount \(amount!) .The discount value is \(newAmount)"))
+        }
+        }
+        
+    }
+
