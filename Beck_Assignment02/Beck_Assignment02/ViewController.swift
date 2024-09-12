c4ba95c31fc9fef71dc89947ff6eb02d8096a457
kylieb233 <145594381+kylieb233@users.noreply.github.com>
2024-09-12 10:49:36 -0500

    saveasssignment

--- Beck_Assignment02/Beck_Assignment02/ViewController.swift
+++ Beck_Assignment02/Beck_Assignment02/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameOL: UITextField!
+    
+    @IBOutlet weak var billOL: UITextField!
+    
+    @IBOutlet weak var tipOL: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitBTN(_ sender: Any) {
+    }
+    
+    @IBAction func resetOL(_ sender: Any) {
+    }
 }
 
