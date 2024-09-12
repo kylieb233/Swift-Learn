b1c7752579809385e5737f3cf2a7d5bda08edba9
kylieb233 <145594381+kylieb233@users.noreply.github.com>
2024-09-12 10:32:30 -0500

    finishedaddingimages

--- DiscountApp/DiscountApp/ViewController.swift
+++ DiscountApp/DiscountApp/ViewController.swift
@@ -23,21 +23,30 @@ class ViewController: UIViewController {
     @IBAction func calculateBtn(_ sender: Any) {
         
         var amount =  Double (inputOL.text!)
-        if(amount! > 0) &&
-            (amount! < 50){
+        if(amount! > 0) && (amount! < 50){
             var newAmount = (amount! * 10/100)
+            
+            displayOL.image = UIImage(named: "kittyboy")
+            
             outputOL.text = ("You recieved 10% discount on the amount \(amount!) .The discount value is \(newAmount)")
-            displayOL.image = 
-        }else if (amount! > 51) &&
-                (amount! < 100){
+            
+        }else if (amount! > 51) && (amount! < 100){
             var newAmount = (amount! * 20/100)
+            
+            displayOL.image = UIImage(named: "shrimpKitty")
+            
             outputOL.text = ("You recieved 20% discount on the amount \(amount!) .The discount value is \(newAmount)")
+
         }
         else {
             var newAmount = (amount! * 30/100)
+            
+            displayOL.image = UIImage(named: "blackcat")
+            
             outputOL.text = (("You recieved 30% discount on the amount \(amount!) .The discount value is \(newAmount)"))
         }
         }
         
     }
+
 
